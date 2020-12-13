e89cd2f10061036f5c804370dec43a4743214e93
ag262 <73322599+ag262@users.noreply.github.com>
2020-12-12 18:51:33 -0600

    Add files via upload

--- NN.py
+++ NN.py
@@ -5,21 +5,17 @@ import os
 from skimage import io
 from skimage import filters
 
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn import metrics
+# def preprocess(x,y):
+# 	x = tf.cast(x,tf.float32)
+# 	y = tf.cast(y,tf.int64)
+# 	return x,y
 
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.svm import LinearSVC, SVC
-from sklearn.metrics import plot_confusion_matrix
-# from mlxtend.plotting import plot_decision_regions
-
-import keras
-from keras.models import Sequential,Input,Model
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers.normalization import BatchNormalization
-from keras.layers.advanced_activations import LeakyReLU
+# def create_dataset(xs, ys, n_classes):
+# 	ys = tf.one_hot(ys,depth=n_classes)
+# 	return tf.data.Dataset.from_tensor_slices((xs, ys)) \
+# 		# .map(preprocess) \
+# 		# .shuffle(len(ys)) \
+# 		.batch(128)
 
 
 
@@ -31,10 +27,11 @@ from keras.layers.advanced_activations import LeakyReLU
 directory = 'data/preprocessed'
 
 # preallocate feature matrix X and label vector d
-num_pixels = 64*64
+num_pixels_x = 64
+num_pixels_y = 64
 num_images = 15000
-X = np.zeros([num_images,num_pixels])
-d = np.zeros([num_images,1])
+X = np.zeros([num_images,num_pixels_x,num_pixels_y])
+d = np.zeros([num_images,])
 
 # fill in X and d
 for (i, f_name) in enumerate(os.listdir(directory)):
@@ -58,14 +55,63 @@ for (i, f_name) in enumerate(os.listdir(directory)):
 	else:
 		label = int(f_name[-5])
 
-	X[i,:] = img.flatten()
+	X[i,:,:] = img
 	d[i] = label
 
 
 
-# split data into nontesting and testing data
-X_nontest, X_test, d_nontest, d_test = train_test_split(X,d,test_size=0.2) # train size is 12000
+from sklearn.model_selection import train_test_split
 
-batch_size = 16
-epochs = 10
-num_classes = 15
+# split data into training and testing data
+X_train, X_test, d_train, d_test = train_test_split(X,d,test_size=0.2) # train size is 12000
+
+
+
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Reshape
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPool2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
+
+# reshape data to have a single channel/color
+X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], X_train.shape[2], 1))
+X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], X_test.shape[2], 1))
+# determine the shape of the input images
+in_shape = X_train.shape[1:]
+print(in_shape)
+
+# preprocess the data a bit
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+d_train -= 1 # to go from [1,15] to [0,14]
+d_test -= 1
+
+# define model
+model = Sequential()
+model.add(Conv2D(60,(6,6),activation='relu',kernel_initializer='he_uniform',input_shape=(64,64,1)))
+model.add(MaxPool2D((3,3)))
+model.add(Flatten())
+model.add(Dense(200, activation='relu', kernel_initializer='he_uniform'))
+model.add(Dropout(0.3))
+model.add(Dense(15, activation='softmax'))
+
+# model.add(Reshape(target_shape=(64*64,),input_shape=(64,64)))
+# model.add(Dense(units=200,activation='relu'))
+# model.add(Dense(units=15,activation='softmax'))
+
+
+
+# define loss and optimizer
+model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+
+# fit the model
+model.fit(X_train,d_train,epochs=10,batch_size=128,verbose=0)
+
+# evaluate the model
+loss,acc = model.evaluate(X_test,d_test,verbose=0)
+
+err = 100*(1-acc)
+print(np.round(err,2))
